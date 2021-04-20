a4ab7da75c7127f83c2cd68658d07c997552f971
nataliyalek <nataliyalek@yandex.ru>
2021-04-20 08:27:58 +0400

    welcam

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,9 +1,25 @@
+<?php  $this->beginPage();?>
 <html>
 <head>
  <title> Project</title>
+ <?php $this->head(); ?>
+</head>
 </head>
 
 <body>
+<?php $this->beginBody();?>
+<?php
+use yii\bootstrap\NavBar;
+
+NavBar::begin([
+        'brandLabel' => 'ProjectYii2',
+        'brandUrl'   => yii::$app->homeUrl,
+
+]);
+NavBar::end();
+?>
+
 <?= $content ?>
+<?php $this->endBody(); ?>
 </body>
 </html>
