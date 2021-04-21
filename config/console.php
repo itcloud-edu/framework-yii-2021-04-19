10308185926a9c00d9d526f8c9624d2276c990bf
leaalex <lea_alex@mail.ru>
2021-04-21 15:10:32 +0500

    add migrate user table

--- config/console.php
+++ config/console.php
@@ -3,6 +3,8 @@
 return [
     'id'=>'Events Manager Console',
     'basePath'=> realpath(__DIR__ . '/../'),
-
+    'components'=>[
+        'db'=> require (__DIR__ . '/db.php')
+    ]
 
     ];
---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'class'=>'yii\db\Connection',
+    'dsn'=> 'mysql:host=localhost;dbname=events_manager',
+    'username'=>'root',
+    'password'=>'root',
+];
