a8a41d6c56fba4f18d7012e924a5a57cf8d86b31
leaalex <lea_alex@mail.ru>
2021-04-21 13:44:41 +0500

    add YII/DEBUG

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 use yii\bootstrap4\NavBar;
 use yii\bootstrap4\Nav;
+use yii\bootstrap4\Button;
 
 
 
@@ -16,15 +17,22 @@ use yii\bootstrap4\Nav;
 
 
 <?php
-NavBar::begin(['brandLabel' => 'Event Manager Project']);
+NavBar::begin([
+        'brandLabel' => 'Event Manager Project',
+        'options' => ['class'=>'navbar navbar-expand-lg navbar-dark bg-dark']
+]);
 echo Nav::widget([
     'items' => [
         ['label' => 'Home', 'url' => ['/site/index']],
         ['label' => 'About', 'url' => ['/site/about']],
         ['label' => 'Login', 'url' => ['/user/login']],
-        ['label' => 'Join', 'url' => ['/user/join']],
+        ['label' => 'Join', 'url' => ['/user/join'] ]
     ],
     'options' => ['class' => 'navbar-nav ml-auto'],
+]);
+echo Button::widget([
+    'label' => 'Action',
+    'options' => ['class' => 'btn btn-primary'],
 ]);
 NavBar::end();
 ?>
