3691feaecf20e3799083a21cc2940cddec7436a3
SimUncle <SimUncle@mail.ru>
2021-04-22 11:07:10 +0400

    Events 0.0.3 (authentication)

--- models/UserJoinForm.php
+++ models/UserJoinForm.php
@@ -8,7 +8,20 @@ class UserJoinForm extends Model{
     public $password;
     public $password2;
 
-
+    public function rules()
+    {
+        // определение обязательных к заполнению полей и проверок на корректность
+        return [
+            ['name', 'required'],
+            ['email', 'required'],
+            ['password', 'required'],
+            ['password2', 'required'],
+            ['name', 'string', 'min' => 4, 'max' => 30],
+            ['email', 'email', 'message' => 'Не верный E-Mail'],
+            ['password', 'string', 'min' => 8, 'max' => 30],
+            ['password2', 'compare', 'compareAttribute' => 'password']
+        ];
+    }
 
 
 }
