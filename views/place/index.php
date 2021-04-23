0b2dc0d1601ef38c14932acf41ee2a7bf85acd07
SimUncle <SimUncle@mail.ru>
2021-04-23 15:24:11 +0400

    Events 0.0.3 (authentication)

--- views/place/index.php
+++ views/place/index.php
@@ -1,12 +1,17 @@
+<?php
+use yii\bootstrap4\Html;
+
+?>
+
 <div class="card mt-5">
     <div class="card-header">
         <h1>Place</h1>
     </div>
     <div class="card-body">
-        <? foreach ($placeList as $place):?>
 
-        <p><?= $place['name'] ?></p>
-        <p><?= $place['addressData'][0]['name'] ?></p>
+            <p><?= $place['name'] ?></p>
+            <p><?= $place['addressData'][0]['name'] ?></p>
+            <?=Html::a('edit','/site/address?action=update&id=' . $address['id'], ['class'=>'btn btn-primary'])?>
             <hr>
         <? endforeach;?>
     </div>
