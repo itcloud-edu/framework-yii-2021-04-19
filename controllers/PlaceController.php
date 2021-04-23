3355c1a6d8b4f01d447f2ed0f43b14513ca5e862
SimUncle <SimUncle@mail.ru>
2021-04-23 15:00:32 +0400

    Events 0.0.3 (authentication)

--- controllers/PlaceController.php
+++ controllers/PlaceController.php
@@ -3,53 +3,54 @@
 namespace app\controllers;
 use app\models\AddressForm;
 use app\models\AddressRecord;
-use app\models\PlaceRecord;
 use yii;
 use yii\base\BaseObject;
 use yii\web\Controller;
 
-class PlaceController extends Controller
+class SiteController extends Controller
 {
     public function actionIndex()
     {
-        $placeList = PlaceRecord::getPlaceList();
-        return $this -> render('index' , compact('placeList'));
+        return $this -> render('index');
+    }
+    public function actionAbout()
+    {
+        return $this -> render('about');
+    }
+    public function actionAddress()
+    {
+        if (Yii::$app->request->isPost)
+            return $this->actionAddressPost();
+
+        $addressForm = new AddressForm();
+
+        if (Yii::$app->request->get('action') == 'update' )
+            $addressForm->name = AddressRecord::findOne(Yii::$app->request->get('id'))->name;
+        elseif (Yii::$app->request->get('action') == 'delete'){
+            AddressRecord::findOne(Yii::$app->request->get('id'))->delete();
+                return $this->redirect('/site/address');
+        }
+
+        $addressList = AddressRecord::getAddressList();
+
+        return $this->render('address',  compact('addressForm', 'addressList'));
     }
 
-//    public function actionAddress()
-//    {
-//        if (Yii::$app->request->isPost)
-//            return $this->actionAddressPost();
-//
-//        $addressForm = new AddressForm();
-//
-//        if (Yii::$app->request->get('action') == 'update' )
-//            $addressForm->name = AddressRecord::findOne(Yii::$app->request->get('id'))->name;
-//        elseif (Yii::$app->request->get('action') == 'delete'){
-//            AddressRecord::findOne(Yii::$app->request->get('id'))->delete();
-//                return $this->redirect('/site/address');
-//        }
-//
-//        $addressList = AddressRecord::getAddressList();
-//
-//        return $this->render('address',  compact('addressForm', 'addressList'));
-//    }
-//
-//    private function actionAddressPost()
-//    {
-//        $addressForm = new AddressForm();
-//        if ($addressForm->load(Yii::$app->request->post()))
-//            if ($addressForm->validate()) {
-//                if (Yii::$app->request->get('id'))
-//                    $addressRecord = AddressRecord::findOne(Yii::$app->request->get('id'));
-//                else
-//                    $addressRecord = new AddressRecord();
-//                $addressRecord->setNameForm($addressForm);
-//                $addressRecord->save();
-//            }
-//        $addressList = AddressRecord::getAddressList();
-//
-//        return $this->render('address',  compact('addressForm', 'addressList'));
-//    }
+    private function actionAddressPost()
+    {
+        $addressForm = new AddressForm();
+        if ($addressForm->load(Yii::$app->request->post()))
+            if ($addressForm->validate()) {
+                if (Yii::$app->request->get('id'))
+                    $addressRecord = AddressRecord::findOne(Yii::$app->request->get('id'));
+                else
+                    $addressRecord = new AddressRecord();
+                $addressRecord->setNameForm($addressForm);
+                $addressRecord->save();
+            }
+        $addressList = AddressRecord::getAddressList();
+
+        return $this->render('address',  compact('addressForm', 'addressList'));
+    }
 
 }
